3209bd997c891dc0444a5a9a2af0920f96dfeb9f
Aditya Mishra <45977202+adimishra6003@users.noreply.github.com>
2023-12-12 01:30:55 +0530

    Medium solved

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,6 +4,14 @@
 */
 
 function isPalindrome(str) {
+  str = str.toLowerCase();
+  str = str.split(" ").join("");
+  str = str.replace(/[^a-z]/g, "");
+  for(let i=0 ; i<str.length ; i++){
+    if(str[i] != str[str.length-1-i]){
+      return false;
+    }
+  }
   return true;
 }
 
