f1feebbc14920b3b6089dcde0373598db4930750
Aditya Mishra <45977202+adimishra6003@users.noreply.github.com>
2023-12-12 01:29:40 +0530

    Easy solved

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,20 @@
 
 function isAnagram(str1, str2) {
 
-}
+  str1 = str1.toLowerCase(str1);
+  str2 = str2.toLowerCase(str2);
 
+  if(str1.length == str2.length){
+    
+    let searchIndex = 0;
+    for(let i=0;i<str1.length;i++){
+      searchIndex = str2.indexOf(str1[i]);
+      if(searchIndex == -1){
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+}
 module.exports = isAnagram;
--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -1,20 +1,37 @@
 /*
   Implement a function `calculateTotalSpentByCategory` which takes a list of transactions as parameter
   and return a list of objects where each object is unique category-wise and has total price spent as its value.
-  transactions is an array where each
-  Transaction - an object like 
-        {
-		id: 1,
-		timestamp: 1656076800000,
-		price: 10,
-		category: 'Food',
-		itemName: 'Pizza',
-	}
-  Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
+  Transaction - an object like { itemName, category, price, timestamp }.
+  Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  if(transactions.length == 0){
+    return [];
+  }
+  let categorySet = new Set();
+  transactions.forEach(item => {
+    categorySet.add(item.category);
+  })
+  categorySet = [...categorySet];
+  
+  let output = [];
+
+  for(let i=0 ; i<categorySet.length ; i++){
+    let obj = {category:categorySet[i], totalSpent:0};
+    // obj[categorySet[i]] = 0;
+    output.push(obj);
+  }
+  for(let i=0 ; i<output.length ; i++){
+    for(let j=0 ; j<transactions.length ; j++){
+      if(output[i]["category"] == transactions[j]["category"]){
+        output[i]["totalSpent"] += transactions[j]["price"];
+      }
+    }
+  }
+
+
+  return output;
 }
 
 module.exports = calculateTotalSpentByCategory;
